e24be5e36b4dda1813d9815285f997f20e5ae8e0
Matheus Oliveira <matheus.oliveira199768@gmail.com>
2022-12-28 13:17:40 -0300

    Complete

---
+++ script.js
@@ -0,0 +1,41 @@
+let pessoa = {
+
+    nome: "Matheus",
+    anoNascimento: "1997",
+    cpf: 14021447736,
+    cidade: "SG",
+    estado: "RJ",
+    logradouro: "avenida",
+
+}
+
+let escola = {
+
+    nome: "Kenzie",
+    cnpj: 1234567890123,
+    area: "TI",
+    cidade: "Curitiba",
+    estado: "PR",
+    logradouro: "rua"
+
+}
+
+let endereço = {
+
+    cidade: "São Gonçalo",
+    estado: "RJ",
+    logradouro: "rua",
+    cep: "123456789"
+
+}
+
+let turma = {
+
+    professores: ["Um", "Dois", "Tres"],
+    alunos: ["Quatro", "Cinco", "Seis"],
+    dataInicio: "30/6/2022",
+    dataFim: "30/6/2023",
+
+}
+
+console.log(turma)
